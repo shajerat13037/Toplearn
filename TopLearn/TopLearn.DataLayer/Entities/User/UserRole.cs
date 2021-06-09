6710c423c0e5ea76f76b0e2453041e00be5b972f
alishajerat <shajerat13037@gmail.com>
2021-06-09 19:20:37 +0430

    Initial and First DataBase

---
+++ TopLearn/TopLearn.DataLayer/Entities/User/UserRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TopLearn.DataLayer.Entities.User
+{
+   public class UserRole
+    {
+        [Key]
+        public int UR_Id { get; set; }
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+
+
+        #region Relations
+
+        public virtual User User { get; set; }
+        public virtual Role Role { get; set; }
+
+        #endregion
+    }
+}
---
+++ TopLearn/TopLearn/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TopLearn.DataLayer.Context;
+
+namespace TopLearn
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+
+            services.AddDbContext<TopLearnContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("TopLearnConnection"));
+            });
+           
+
+          
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                     name: "default",
+                     pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        
+        }
+    }
+}
